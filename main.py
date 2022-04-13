9d7c31eb260a280293ac5f31c5b91a76e3497cce
NoelP <64641984+NoelisTired@users.noreply.github.com>
2022-04-13 15:54:49 +0200

    Update main.py

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ from threading import Thread
 print(Colorate.Horizontal(Colors.red_to_white, Center.XCenter(f"[NoelP X] Welcome to the (open-source) client to interect with a lil project that has been worked on"), 1))
 print(Colorate.Horizontal(Colors.red_to_white, Center.XCenter(f"As of right now there's a ratelimit of 10.000 shares/day\n\n"), 1))
 videoid = Write.Input(Center.XCenter("Enter your VideoID > "), Colors.red_to_white, interval=0.0100) #If any braindead fuck inputs a full url here i'm not resetting your ratelimit
-videoid = Write.Input(Center.XCenter("Enter your key: (noelp.live) "), Colors.red_to_white, interval=0.0100)
+key = Write.Input(Center.XCenter("Enter your key: (noelp.live) "), Colors.red_to_white, interval=0.0100)
 while True:
     response = requests.get(f"https://noelp-backend.xyz/shares?id={str(videoid)}&key={key}")
     if response.status_code == 200:
