e3823a2f25654c9f9bebe9a0aa38168bdaf713d7
NoelP <64641984+NoelisTired@users.noreply.github.com>
2022-04-13 15:50:58 +0200

    Update main.py

--- main.py
+++ main.py
@@ -15,8 +15,6 @@ from pystyle import Colors, Colorate, Add, Center, Box, Write
 import threading
 from threading import Thread
 
-amount = 10 #Multiply this amount by 1000 to get total views
-
 print(Colorate.Horizontal(Colors.red_to_white, Center.XCenter(f"[NoelP X] Welcome to the (open-source) client to interect with a lil project that has been worked on"), 1))
 print(Colorate.Horizontal(Colors.red_to_white, Center.XCenter(f"As of right now there's a ratelimit of 10.000 shares/day\n\n"), 1))
 videoid = Write.Input(Center.XCenter("Enter your VideoID > "), Colors.red_to_white, interval=0.0100) #If any braindead fuck inputs a full url here i'm not resetting your ratelimit
