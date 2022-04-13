1302cb978b99b94a805c36c08ece029b7f268eea
NoelP <64641984+NoelisTired@users.noreply.github.com>
2022-04-13 15:46:58 +0200

    Update main.py

--- main.py
+++ main.py
@@ -20,8 +20,9 @@ amount = 10 #Multiply this amount by 1000 to get total views
 print(Colorate.Horizontal(Colors.red_to_white, Center.XCenter(f"[NoelP X] Welcome to the (open-source) client to interect with a lil project that has been worked on"), 1))
 print(Colorate.Horizontal(Colors.red_to_white, Center.XCenter(f"As of right now there's a ratelimit of 10.000 shares/day\n\n"), 1))
 videoid = Write.Input(Center.XCenter("Enter your VideoID > "), Colors.red_to_white, interval=0.0100) #If any braindead fuck inputs a full url here i'm not resetting your ratelimit
+videoid = Write.Input(Center.XCenter("Enter your key: (noelp.live) "), Colors.red_to_white, interval=0.0100)
 while True:
-    response = requests.get(f"https://noelp-backend.xyz/shares?id={str(videoid)}")
+    response = requests.get(f"https://noelp-backend.xyz/shares?id={str(videoid)}&key={key}")
     if response.status_code == 200:
         print("We've sent 1000 Shares!")
         
